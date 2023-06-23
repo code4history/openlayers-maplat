0ddc8760f77cc7c318203653ad1ec4652a68bc53
kochizufan <kochizufan@gmail.com>
2023-06-24 07:47:08 +0900

    Define types

---
+++ src/ol/maplat/types/basics.js
@@ -0,0 +1,23 @@
+/**
+ * @module ol/maplat/types/basics
+ */
+
+/**
+ * @typedef {{[key: string]: string}} LocaleDictFragment
+ */
+
+/**
+ * @typedef { LocaleDictFragment | string } LocaleFragment
+ */
+
+/**
+ * @typedef { [number, number] } Coordinate2D
+ */
+
+/**
+ * @typedef { "All right reserved" | "CC BY" | "CC BY-SA" | "CC BY-ND" | "CC BY-NC" | "CC BY-NC-SA" | "CC BY-NC-ND" | "CC0" } DataLicense
+ */
+
+/**
+ * @typedef { DataLicense | "PD" } MapLicense
+ */
--- src/ol/maplat/types/specFile.js
+++ src/ol/maplat/types/specFile.js
@@ -1,27 +1,11 @@
 /**
- * @module ol/maplat/types
- */
-
-/** 
- * @typedef { {[key: String]: String} } LocaleDictFragment
- */
-
-/** 
- * @typedef { LocaleDictFragment | String } LocaleFragment
- */
-
-/** 
- * @typedef { [number, number] } Coordinate2D
- */
-
-/** 
- * @typedef { "All right reserved" | "CC BY" | "CC BY-SA" | "CC BY-ND" | "CC BY-NC" | "CC BY-NC-SA" | "CC BY-NC-ND" | "CC0" } LicenseSelection
+ * @module ol/maplat/types/specFile
  */
 
 /**
  * @typedef { Object } MaplatMetaData
- * @property { String } lang Default language
- * @property { String } mapID ID of map
+ * @property { string } lang Default language
+ * @property { string } mapID ID of map
  * @property { LocaleFragment } title Title of Map (In short)
  * @property { LocaleFragment } [officialTitle] Official Title of Map (In long)
  * @property { LocaleFragment } attr Attribution of map image
@@ -30,12 +14,12 @@
  * @property { LocaleFragment } [contributor] Contributor/Owner of the map
  * @property { LocaleFragment } [mapper] Mapper of the mapping
  * @property { LocaleFragment } [description] Description of map
- * @property { LicenseSelection | "PD" } license License of map
- * @property { LicenseSelection } dataLicense License of mapping data
+ * @property { MapLicense } license License of map
+ * @property { DataLicense } dataLicense License of mapping data
  * @property { LocaleFragment } createdAt The date of map was created
  * @property { LocaleFragment } [era] The era described in the map
- * @property { String } reference Reference or source of map image
- * 
+ * @property { string } reference Reference or source of map image
+ *
  */
 
 /**
@@ -46,33 +30,33 @@
  * @property { number } yScale Scale factor of Y param - e
  * @property { number } xOrigin Origin factor of X param - c
  * @property { number } yOrigin Origin factor of Y param - f
- * 
+ *
  */
 
 /**
  * @typedef { Object } MaplatProjectionSpec
  * @property { Coordinate2D } [size] Size of image in pixel
- * @property { String } mapCoord Coordinate name of map
+ * @property { string } mapCoord Coordinate name of map
  * @property { MaplatWorldParams } [worldParams] World parameter transforming pixel to map coord
- * @property { String } interOperationCode Coordinate for coordinate inter operability
- * 
+ * @property { string } interOperationCode Coordinate for coordinate inter operability
+ *
  */
 
 /**
  * @typedef { Object } MaplatSourceSpec
  * @property { "PIXEL" | "WMTS" | "TMS" | "IIIF" } tileSourceType Type of tile source
  * @property { "WARP" | "NONE" } warp Do map image warping or not
- * @property { String } [url] URL of template of image tile data 
- * @property { String } extension String of image's extension
- * 
+ * @property { string } [url] URL of template of image tile data
+ * @property { string } extension String of image's extension
+ *
  */
 
 /**
  * @typedef { Object } MaplatDefinition
- * @property { String } version Maplat definition data scheme version
- * @property { String } mapID ID of map
+ * @property { string } version Maplat definition data scheme version
+ * @property { string } mapID ID of map
  * @property { MaplatMetaData } metaData Maplat's metadata of image
  * @property { MaplatProjectionSpec } projectionSpec Map's projection spec
  * @property { MaplatSourceSpec } sourceSpec Map's source spec
- * 
+ *
  */
